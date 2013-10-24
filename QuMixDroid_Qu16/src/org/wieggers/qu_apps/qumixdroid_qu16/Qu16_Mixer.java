5dde048fd905a5bd3a5c8f2a35cbfb215cecabb8
George Wieggers <george@wieggers.org>
2013-10-24 19:22:54 +0200

    Commit several modifications before starting on login screen.

--- QuMixDroid_Qu16/src/org/wieggers/qu_apps/qumixdroid_qu16/Qu16_Mixer.java
+++ QuMixDroid_Qu16/src/org/wieggers/qu_apps/qumixdroid_qu16/Qu16_Mixer.java
@@ -1,10 +1,20 @@
 package org.wieggers.qu_apps.qumixdroid_qu16;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 import org.wieggers.qu_apps.qumixdroid_communication.Connected_Device;
 import org.wieggers.qu_apps.qumixdroid_communication.IDeviceListener;
+
+import android.content.Context;
 
 /**
  * Qu16_Mixer is a "virtual" mixer which holds an active "scene" in memory, corresponding with the Qu-16 it is connected to
@@ -24,14 +34,18 @@ public class Qu16_Mixer implements IDeviceListener, IMixValueListener, IParserLi
 	 * @param remoteIp	IP address of Qu-16
 	 * @param port		Port of Qu-16
 	 * @param demoMode	The virtual mixer can be initialized with some fake data, so the app can be used in demo mode
+	 * @throws IOException 
 	 */
-	public Qu16_Mixer(String remoteIp, int port, boolean demoMode) {
+	public Qu16_Mixer(Context context, String remoteIp, int port, boolean demoMode) throws IOException {
 		
 		mListenerLock = new Object();
 		mListeners = new LinkedList<IMixerListener>();
 		mParser = new Qu16_Command_Parser(Qu16_Command_Direction.from_qu_16);
 		mMixValues = new HashMap<String, Qu16_MixValue>();
-				
+		
+		//AssetManager assetManager = context.getAssets();
+		//readScene(assetManager.open("qu16_init_scene.txt"));
+		
 		if (!demoMode) {
 			mDevice = new Connected_Device(remoteIp, port);
 		} else {
@@ -98,5 +112,38 @@ public class Qu16_Mixer implements IDeviceListener, IMixValueListener, IParserLi
 			}
 		}
 		
+	}
+	
+	public void readScene(InputStream is) throws NumberFormatException, IOException 
+	{	
+		BufferedReader r = new BufferedReader(new InputStreamReader(is));
+		
+		String line;
+		while ((line = r.readLine()) != null)
+		{
+			String[] parts = line.split(",");	
+			 
+			byte[] bytes = new byte[parts.length];
+			int count = 0;
+			for(String str : parts)
+			{
+			    bytes[count++] = Byte.parseByte(str);
+			}
+		}
+	}
+	
+	public void writeScene(OutputStream os) throws IOException
+	{
+		BufferedWriter w = new BufferedWriter(new OutputStreamWriter(os));
+		
+		for (HashMap.Entry<String, Qu16_MixValue> mixValue : mMixValues.entrySet()) {
+			byte[] singleCmd = mixValue.getValue().getCommand(Qu16_Command_Direction.from_qu_16);
+			
+			String line = Arrays.toString(singleCmd);
+			line = line.replaceAll("[\\[|\\]| ]", ""); // [1, 2, 3, 4] ==> 1,2,3,4
+			
+			w.write(line);
+			w.write("\n");
+		}		
 	}
 }
