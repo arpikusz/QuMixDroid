692de83a7f4f6332e3051b2a5a37611eec3791be
George Wieggers <george@wieggers.org>
2013-11-02 10:18:46 +0100

    Layout

--- QuMixDroid_Qu16/src/org/wieggers/qu_apps/qu16/midi/Qu16_VA_Ranges.java
+++ QuMixDroid_Qu16/src/org/wieggers/qu_apps/qu16/midi/Qu16_VA_Ranges.java
@@ -1,61 +1,73 @@
 package org.wieggers.qu_apps.qu16.midi;
 
+import android.annotation.SuppressLint;
 import java.util.HashMap;
 
+@SuppressLint("UseSparseArrays")
 public class Qu16_VA_Ranges {
+	
+	public static final HashMap<Integer, HashMap<Integer, Qu16_Input_Channels>> Mixer_Layout;
 	public static final HashMap<Integer, String> Fader_Send;
-	static {
-		HashMap<Integer, String> initMap = new HashMap<Integer, String>();
-		initMap.put(0x7F, "10");
-		initMap.put(0x74, "5");
-		initMap.put(0x6B, "0");
-		initMap.put(0x61, "5");
-		initMap.put(0x57, "10");
-		initMap.put(0x43, "20");
-		initMap.put(0x2F, "30");
-		initMap.put(0x1B, "40");
-		initMap.put(0, "∞");
-		Fader_Send = initMap;
-	}
-	
 	public static final HashMap<Integer, String> Local_Gain;
-	static {
-		HashMap<Integer, String> initMap = new HashMap<Integer, String>();
-		initMap.put(0x7F, "60");
-		initMap.put(0x6B, "50");
-		initMap.put(0x57, "40");
-		initMap.put(0x44, "30");
-		initMap.put(0x30, "20");
-		initMap.put(0x1D, "10");
-		initMap.put(0x13, "5");
-		initMap.put(0x0A, "0");
-		initMap.put(0x00, "5");
-		Local_Gain = initMap;
-	}
-	
 	public static final HashMap<Integer, String> dSnake_Gain;
-	static {
-		HashMap<Integer, String> initMap = new HashMap<Integer, String>();
-		initMap.put(0x7F, "60");
-		initMap.put(0x67, "50");
-		initMap.put(0x50, "40");
-		initMap.put(0x45, "35");
-		initMap.put(0x39, "30");
-		initMap.put(0x2E, "25");
-		initMap.put(0x22, "20");
-		initMap.put(0x0B, "10");
-		initMap.put(0x00, "5");
-		dSnake_Gain = initMap;
-	}
-
 	public static final HashMap<Integer, String> Compressor_Type;
+
 	static {
-		HashMap<Integer, String> initMap = new HashMap<Integer, String>();
-		initMap.put(0x00, "Manual Peak");
-		initMap.put(0x01, "Manual RMS");
-		initMap.put(0x02, "Auto Slow Opto");
-		initMap.put(0x03, "Auto Punchbag");
-		Compressor_Type = initMap;
+		HashMap<Integer, String> init_Fader = new HashMap<Integer, String>();
+		init_Fader.put(0x7F, "10");
+		init_Fader.put(0x74, "5");
+		init_Fader.put(0x6B, "0");
+		init_Fader.put(0x61, "5");
+		init_Fader.put(0x57, "10");
+		init_Fader.put(0x43, "20");
+		init_Fader.put(0x2F, "30");
+		init_Fader.put(0x1B, "40");
+		init_Fader.put(0, "∞");
+		Fader_Send = init_Fader;
+
+		HashMap<Integer, String> init_Gain = new HashMap<Integer, String>();
+		init_Gain.put(0x7F, "60");
+		init_Gain.put(0x6B, "50");
+		init_Gain.put(0x57, "40");
+		init_Gain.put(0x44, "30");
+		init_Gain.put(0x30, "20");
+		init_Gain.put(0x1D, "10");
+		init_Gain.put(0x13, "5");
+		init_Gain.put(0x0A, "0");
+		init_Gain.put(0x00, "5");
+		Local_Gain = init_Gain;
+
+		HashMap<Integer, String> init_Snake_Gain = new HashMap<Integer, String>();
+		init_Snake_Gain.put(0x7F, "60");
+		init_Snake_Gain.put(0x67, "50");
+		init_Snake_Gain.put(0x50, "40");
+		init_Snake_Gain.put(0x45, "35");
+		init_Snake_Gain.put(0x39, "30");
+		init_Snake_Gain.put(0x2E, "25");
+		init_Snake_Gain.put(0x22, "20");
+		init_Snake_Gain.put(0x0B, "10");
+		init_Snake_Gain.put(0x00, "5");
+		dSnake_Gain = init_Snake_Gain;
+
+		HashMap<Integer, String> init_Compressor = new HashMap<Integer, String>();
+		init_Compressor.put(0x00, "Manual Peak");
+		init_Compressor.put(0x01, "Manual RMS");
+		init_Compressor.put(0x02, "Auto Slow Opto");
+		init_Compressor.put(0x03, "Auto Punchbag");
+		Compressor_Type = init_Compressor;
+		
+		HashMap<Integer, HashMap<Integer, Qu16_Input_Channels>> init_Layout = new HashMap<Integer, HashMap<Integer, Qu16_Input_Channels>>();
+		init_Layout.put(2, new HashMap<Integer, Qu16_Input_Channels>());
+		
+		HashMap<Integer, Qu16_Input_Channels> layer1 = new HashMap<Integer, Qu16_Input_Channels>(); 
+		for (byte i = Qu16_Input_Channels.Mono_01.getValue(); i <= Qu16_Input_Channels.Mono_16.getValue(); ++i) {
+			layer1.put((int) i, Qu16_Input_Channels.fromValue(i));
+		}
+		init_Layout.put(1, layer1);
+		
+		
+		
+		Mixer_Layout = init_Layout;
 	}
 
 }
